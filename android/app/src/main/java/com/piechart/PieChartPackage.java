5e4cc3ff3fc768832126d570c7b5173afdcd72fa
Mike Mendizabal <mendizam@gmatc.matc.edu>
2022-05-12 23:15:43 -0500

    Initial commit

---
+++ android/app/src/main/java/com/piechart/PieChartPackage.java
@@ -0,0 +1,27 @@
+package com.piechart;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PieChartPackage implements ReactPackage {
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList(
+                new PieChartManager()
+        );
+    }
+
+    @Override
+    public List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+                new MainReactPackage(), new PieChartPackage()
+        );
+    }
+}
